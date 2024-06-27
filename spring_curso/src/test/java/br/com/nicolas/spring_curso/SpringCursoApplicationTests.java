b242f65bada33b45bf6887e70427a64e8c0123db
magrininicolas <magrininicolas@outlook.com>
2024-06-27 14:32:14 -0300

    initial commit

---
+++ spring_curso/src/test/java/br/com/nicolas/spring_curso/SpringCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.spring_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
